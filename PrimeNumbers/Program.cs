1783749a75127f8562fc1de81c41d3703a098f59
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-02-05 11:27:23 +0100

    Add prime numbers exercise solution

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+const int startNumber = 1;
+const int endNumber = 1000;
+
+Console.WriteLine($"The prime numbers between {startNumber} and {endNumber} are:");
+
+var numbers = Enumerable
+    .Range(startNumber, endNumber - startNumber)
+    .Where(IsPrime);
+
+Console.WriteLine(string.Join(", ", numbers));
+Console.WriteLine("\nPress Enter to exit");
+Console.ReadLine();
+
+static bool IsPrime(int number)
+{
+    bool CalculatePrime(int value)
+    {
+        var possibleFactors = Math.Sqrt(number);
+        for (var factor = 2; factor <= possibleFactors; factor++)
+        {
+            if (value % factor == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return number > 1 && CalculatePrime(number);
+}
