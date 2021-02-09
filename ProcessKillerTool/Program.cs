e3ae8dc8b809f20a009ee87e62bb46f2fd51f3c1
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-02-09 08:18:53 +0100

    Add process killer tool exercise

---
+++ ProcessKillerTool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProcessKillerTool
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var processesToKill = new List<Process>();
+
+            foreach (var arg in args)
+            {
+                processesToKill.AddRange(Process.GetProcessesByName(arg));
+            }
+
+            foreach (var process in processesToKill)
+            {
+                process.Kill();
+            }
+        }
+    }
+}
