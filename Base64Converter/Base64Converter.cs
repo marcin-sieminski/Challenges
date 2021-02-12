c27ee7befc9fb2f43473d91acc981a8fd2e1268b
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-02-12 16:33:01 +0100

    Add Base64 converter exercise

---
+++ Base64Converter/Base64Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Base64Converter
+{
+    public static class Base64Converter
+    {
+        public static void ConvertFileToBase64(string inputFileName, string encodedFileName)
+        {
+            var originalBytes = File.ReadAllBytes(inputFileName);
+            var base64String = Convert.ToBase64String(originalBytes);
+            File.WriteAllText(encodedFileName, base64String);
+        }
+
+        public static void ConvertFileFromBase64(string encodedFileName, string decodedFileName)
+        {
+            var base64String = File.ReadAllText(encodedFileName);
+            var fileBytes = Convert.FromBase64String(base64String);
+            File.WriteAllBytes(decodedFileName, fileBytes);
+        }
+        public static string ConvertTextToBase64(string inputText)
+        {
+            var originalBytes = Encoding.Default.GetBytes(inputText);
+            return Convert.ToBase64String(originalBytes);
+        }
+
+        public static string ConvertTextFromBase64(string encodedText)
+        {
+            var textBytes = Convert.FromBase64String(encodedText);
+            return Encoding.Default.GetString(textBytes);
+        }
+    }
+}
---
+++ Base64Converter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Base64Converter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a text to encode (Base64):");
+            var textToEncode = Console.ReadLine();
+
+            var encodedText = Base64Converter.ConvertTextToBase64(textToEncode);
+            var decodedText = Base64Converter.ConvertTextFromBase64(encodedText);
+
+            Console.WriteLine("\nText to encode:");
+            Console.WriteLine(textToEncode);
+            Console.WriteLine("Encoded text (Base64):");
+            Console.WriteLine(encodedText);
+            Console.WriteLine("Decoded text:");
+            Console.WriteLine(decodedText);
+
+            //EncodeDecodeBinaryFileExample();
+
+            Console.WriteLine("\nPress enter do exit.");
+            Console.ReadLine();
+        }
+
+        private static void EncodeDecodeBinaryFileExample()
+        {
+            const string inputFileName = "sun.png";
+            const string encodedFileName = "sun.txt";
+            const string decodedFileName = "sun_decoded.png";
+
+            Base64Converter.ConvertFileToBase64(inputFileName, encodedFileName);
+            Base64Converter.ConvertFileFromBase64(encodedFileName, decodedFileName);
+        }
+    }
+}
