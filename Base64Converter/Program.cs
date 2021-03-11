3b7d5c31b5827ad8be88e4939e3fc4025803872d
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-03-11 22:32:52 +0100

    Improved Base64Converter console UI

--- Base64Converter/Program.cs
+++ Base64Converter/Program.cs
@@ -6,20 +6,23 @@ namespace Base64Converter
     {
         public static void Main()
         {
-            var runAgain  = true;
+            var runAgain = true;
             do
             {
                 Console.Clear();
-                Console.WriteLine("Choose an option: \n1. Encode text \n2. Encode binary file \n3. Exit");
+                Console.WriteLine("Choose an option: \n1. Encode text \n2. Decode text \n3. Encode binary file \n0. Exit");
                 var option = Console.ReadKey();
                 Console.WriteLine();
 
                 switch (option.KeyChar)
                 {
                     case '1':
-                        EncodeDecodeText();
+                        EncodeText();
                         break;
                     case '2':
+                        DecodeText();
+                        break;
+                    case '3':
                         EncodeDecodeBinaryFile();
                         break;
                     default:
@@ -29,19 +32,33 @@ namespace Base64Converter
             } while (runAgain);
         }
 
-        private static void EncodeDecodeText()
+        private static void EncodeText()
         {
             Console.Clear();
             Console.WriteLine("Enter a text to encode (Base64):");
             var textToEncode = Console.ReadLine();
 
             var encodedText = Base64Converter.ConvertTextToBase64(textToEncode);
-            var decodedText = Base64Converter.ConvertTextFromBase64(encodedText);
 
             Console.WriteLine("\nText to encode:");
             Console.WriteLine(textToEncode);
             Console.WriteLine("\nEncoded text (Base64):");
             Console.WriteLine(encodedText);
+
+            Console.WriteLine("\nPress enter to continue.");
+            Console.ReadLine();
+        }
+
+        private static void DecodeText()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter a text to decode (Base64):");
+            var textToDecode = Console.ReadLine();
+
+            var decodedText = Base64Converter.ConvertTextFromBase64(textToDecode);
+
+            Console.WriteLine("\nText to decode:");
+            Console.WriteLine(textToDecode);
             Console.WriteLine("\nDecoded text:");
             Console.WriteLine(decodedText);
 
@@ -52,8 +69,13 @@ namespace Base64Converter
         private static void EncodeDecodeBinaryFile()
         {
             Console.Clear();
-            Console.WriteLine("Enter an input file name to encode (Base64):");
-            var inputFileName = Console.ReadLine();
+            string inputFileName;
+            do
+            {
+                Console.WriteLine("Enter an input file name to encode (Base64):");
+                inputFileName = Console.ReadLine();
+
+            } while (string.IsNullOrEmpty(inputFileName));
 
             var encodedFileName = inputFileName + ".txt";
             var decodedFileName = "decoded_" + inputFileName;
