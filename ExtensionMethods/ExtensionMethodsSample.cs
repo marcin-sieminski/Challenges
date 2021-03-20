9261249d05997073b6492af170ef20195c8b2a3e
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-03-20 22:48:24 +0100

    Add extenstion method exercise

---
+++ ExtensionMethods/ExtensionMethodsSample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethodsSample
+    {
+        public static void Print(this string stringToPrint)
+        {
+            Console.WriteLine(stringToPrint);
+        }
+
+        public static string Excite(this string stringToExcite)
+        {
+            return stringToExcite.Replace('.', '!');
+        }
+
+        public static Person Fill(this Person inputPerson)
+        {
+            Console.Write("Enter first name: ");
+            inputPerson.FirstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            inputPerson.LastName = Console.ReadLine();
+            Console.Write("Enter age: ");
+           var ageInputString = Console.ReadLine();
+           inputPerson.Age = int.Parse(ageInputString);
+           return inputPerson;
+        }
+
+        public static Person Print(this Person inputPerson)
+        {
+            Console.WriteLine($"{inputPerson.FirstName} {inputPerson.LastName} is {inputPerson.Age} years old");
+            return inputPerson;
+        }
+
+        public static double Add(this double input, double x)
+        {
+            return input + x;
+        }
+
+        public static double Subtract(this double input, double x)
+        {
+            return input - x;
+        }
+
+        public static double MultiplyBy(this double input, double x)
+        {
+            return input * x;
+        }
+        
+        public static double DivideBy(this double input, double x)
+        {
+            return input / x;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sampleText = "Hello World...";
+            sampleText.Excite().Print();
+
+            var person = new Person();
+            person.Fill().Print();
+
+            Console.WriteLine(8.12.Add(4).Subtract(2).MultiplyBy(5).DivideBy(6.6));
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
